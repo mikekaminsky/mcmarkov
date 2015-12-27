77e7e2596e2bf49c756b121d4d4a58e7a78c30f0
Michael Kaminsky <michaelkaminsky@Michaels-MacBook-Air.local>
2015-12-27 11:01:04 -0500

    Updates demo script

--- demo.py
+++ demo.py
@@ -2,18 +2,23 @@ from markov.markov import MarkovChain
 import pickle
 
 corpus = pickle.load(open( "eminemcorpus.pickle", "rb" ) )
-mc = MarkovChain(corpus, 2)
+mc = MarkovChain(corpus, 4)
 print "Fitting..."
 mc.fit()
 print "Fit complete!"
 
 generated_lyrics = []
 word = None
-for i in range(0,10):
+print('starting lyric generation!')
+for i in range(0,50):
+    print i
     if word:
         # NOTE: this argument has to a be a list
         word = mc.next_word([word])
+        print word
     else:
         word = mc.next_word()
-    print word
+        print word
     generated_lyrics.append(word)
+
+print(generated_lyrics)
