9c3a13729ad3a91d281c3335bdb65be36ea04927
Michael Kaminsky <michaelkaminsky@Michaels-Air.home>
2016-01-18 13:49:39 -0500

    Adds ability to create songs of a given number of couplets.
    Unit tests are passing, but needs refactoring. Still need to add
    ability to specify syllable count.

--- mc/mc.py
+++ mc/mc.py
@@ -1,8 +1,10 @@
 import sys
 from os import path
 sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+import random
 
 from markov.markov import MarkovChain
+from rhymes.rhymes import rhymesyls
 
 
 # TODO:
@@ -13,6 +15,8 @@ from markov.markov import MarkovChain
 #     [ ] Write a method for building raps using couplets
 #  [ ] Ability to specify the syllable count of a line
 #  [ ] Option to 'clean' the corpus by removing certain punctuation
+
+
 
 class MCMarkov():
 
@@ -25,8 +29,41 @@ class MCMarkov():
         self.markovchain.fit()
         self.starting_words = [line[0] for line in self.corpus]
         self.rhymedict = {}
-
+        for word in self.starting_words:
+            keys = rhymesyls(word)
+            for key in keys:
+                if key in self.rhymedict:
+                    self.rhymedict[key].append(word)
+                else:
+                    self.rhymedict[key] = [word]
+        self.words_to_rhyme = {}
+        for key in self.rhymedict.keys():
+            if len(self.rhymedict[key])>1:
+                self.words_to_rhyme[key] = self.rhymedict[key]
 
 
     def create_song(self, couplets, syllables):
-        pass
+        song = []
+        for i in range(0,couplets):
+            rhymewords = set(self.words_to_rhyme[random.choice(self.words_to_rhyme.keys())])
+            thisrhymes = random.sample(rhymewords,2)
+            startingword1 = thisrhymes[0]
+            startingword2 = thisrhymes[1]
+            #Line 1
+            line1 = [startingword1]
+            line2 = [startingword2]
+            for j in range(0,5):
+                if j == 0:
+                    word = self.markovchain.next_word([startingword1])
+                else:
+                    word = self.markovchain.next_word([word])
+                line1.append(word)
+            for j in range(0,5):
+                if j == 0:
+                    word = self.markovchain.next_word([startingword2])
+                else:
+                    word = self.markovchain.next_word([word])
+                line2.append(word)
+            couplet = [line1[::-1], line2[::-1]]
+            song.append(couplet)
+        return song
--- mc/test.py
+++ mc/test.py
@@ -4,6 +4,7 @@ sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 import unittest
 
 from mc import MCMarkov
+from rhymes.rhymes import rhymes_with
 
 class TestMC(unittest.TestCase):
 
@@ -12,6 +13,26 @@ class TestMC(unittest.TestCase):
                 ['alpha','beta','gamma','delta','alpha','gamma','epsilon'],
                 ['alef','bet','gimel','dalet','alef','gimel']
                         ]
+
+        self.where_the_sidewalk_ends = [
+                    ['there', 'is', 'a', 'place', 'where', 'the', 'sidewalk', 'ends'],
+                    ['and', 'before', 'the', 'street', 'begins'],
+                    ['and', 'there', 'the', 'grass', 'grows', 'soft', 'and', 'white'],
+                    ['and', 'there', 'the', 'sun', 'burns', 'crimson', 'bright'],
+                    ['and', 'there', 'the', 'moon-bird', 'rests', 'from', 'his', 'flight'],
+                    ['to', 'cool', 'in', 'the', 'peppermint', 'wind'],
+                    ['let', 'us', 'leave', 'this', 'place', 'where', 'the', 'smoke', 'blows', 'black'],
+                    ['and', 'the', 'dark', 'street', 'winds', 'and', 'bends'],
+                    ['past', 'the', 'pits', 'where', 'the', 'asphalt', 'flowers', 'grow'],
+                    ['we', 'shall', 'walk', 'with', 'a', 'walk', 'that', 'is', 'measured', 'and', 'slow'],
+                    ['and', 'watch', 'where', 'the', 'chalk-white', 'arrows', 'go'],
+                    ['to', 'the', 'place', 'where', 'the', 'sidewalk', 'ends'],
+                    ['yes', "we'll", 'walk', 'with', 'a', 'walk', 'that', 'is', 'measured', 'and', 'slow'],
+                    ['and', "we'll", 'go', 'where', 'the', 'chalk-white', 'arrows', 'go'],
+                    ['for', 'the', 'children,', 'they', 'mark,', 'and', 'the', 'children,', 'they', 'know'],
+                    ['the', 'place', 'where', 'the', 'sidewalk', 'ends']
+                ]
+
         self.flattened_corpus = [item for sublist in self.corpus for item in sublist]
 
     def test_MC_creates_MarkovChain_object(self):
@@ -30,31 +51,49 @@ class TestMC(unittest.TestCase):
     def test_starting_word_rhyme_dictionary_contains_all_first_words(self):
         MC = MCMarkov(self.corpus, 1, True)
         rhymedict_values = MC.rhymedict.values()
-        self.assertEqual(set([item for sublist in rhymedict_values for item in sublist]), set([item for sublist in self.corpus for item in sublist]))
+        self.assertEqual(set([item for sublist in rhymedict_values for item in sublist]), set(MC.starting_words))
 
     def test_starting_word_rhyme_dictionary_can_have_multiple_pronunciations(self):
         MC = MCMarkov([['hated'],['mated']], 1, True)
         rhymedict_values = [item for sublist in MC.rhymedict.values() for item in sublist]
         self.assertGreater(rhymedict_values.count('hated'),1)
 
-    def test_chooses_random_starting_rhyme_with_at_least_two_words(self):
-        pass
+    def test_words_to_rhyme_all_have_two_words(self):
+        MC = MCMarkov([['hated'],['mated'],['boat']], 1, True)
+        self.assertFalse('boat' in [item for sublist in MC.words_to_rhyme.values() for item in sublist])
+        self.assertTrue('hated' in [item for sublist in MC.words_to_rhyme.values() for item in sublist])
+        self.assertTrue('mated' in [item for sublist in MC.words_to_rhyme.values() for item in sublist])
 
-    def test_chooses_random_starting_word_from_rhyme_group(self):
-        pass
+    def test_song_contains_correct_number_of_couplets(self):
+        MC = MCMarkov(self.where_the_sidewalk_ends, 1, True)
+        song = MC.create_song(4, 10)
+        self.assertEqual(len(song),4)
+
+    def test_couplets_are_not_empty(self):
+        MC = MCMarkov(self.where_the_sidewalk_ends, 1, True)
+        song = MC.create_song(4, 10)
+        for couplet in song:
+            for line in couplet:
+                self.assertGreater(len(line),0)
 
     def test_rhymes_within_couplet(self):
-        pass
+        MC = MCMarkov(self.where_the_sidewalk_ends, 1, True)
+        song = MC.create_song(4, 10)
+        for couplet in song:
+            endword1 = couplet[0][-1]
+            endword2 = couplet[1][-1]
+            self.assertTrue(rhymes_with(endword1, endword2))
 
-    def test_same_word_does_not_end_adjacent_lines(self):
-        pass
+    def test_lines_in_couplet_do_not_end_with_same_word(self):
+        MC = MCMarkov(self.where_the_sidewalk_ends, 1, True)
+        song = MC.create_song(4, 10)
+        for couplet in song:
+            endword1 = couplet[0][-1]
+            endword2 = couplet[1][-1]
+            self.assertNotEqual(endword1, endword2)
 
     def test_corpus_must_have_at_least_two_rhyming_starting_words(self):
         pass
-
-    def test_all_words_in_corpus_have_syllable_count(self):
-        pass
-
 
 if __name__ == '__main__':
     unittest.main()
