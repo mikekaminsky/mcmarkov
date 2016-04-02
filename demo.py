8258a2cf9e82508fe74a0299be1672cce412063c
Michael Kaminsky <michaelkaminsky@Michaels-Air.home>
2016-04-02 15:46:47 -0400

    Working end-to-end demo :)

--- demo.py
+++ demo.py
@@ -1,10 +1,41 @@
 from mc.mc import MCMarkov
-import pickle
+import sqlite3
+from scraper.scraper import Scraper
+from scraper.dbconfig import DBConfig
 
-corpus = pickle.load(open( "eminemcorpus.pickle", "rb" ) )
+
+# Run these lines if you are just getting started
+#d = DBConfig()
+#d.db_destroy()
+#d.db_create()
+#s = Scraper()
+#s.add_songs(['gucci mane'])
+#s.add_lyrics()
+
+def getlyrics(cursor, artist):
+    SQL = "SELECT line from lyrics join songs on lyrics.song_id = songs.id where artist =?;" # Note: no quotes
+    data = (artist, )
+    cursor.execute(SQL, data)
+    lyrics = cursor.fetchall()
+    return [i[0].encode('UTF-8').decode('UTF-8').lower().split() for i in lyrics]
+
+conn=sqlite3.connect("/usr/local/sqlite/rapgenerator.db")
+c = conn.cursor()
+corpus = getlyrics(c, 'gucci mane')
+
 mc = MCMarkov(corpus, 4, True)
 new_song = mc.create_song(couplet_count=10, syllable_count=10)
 
 for couplet in new_song:
     for line in couplet:
         print ' '.join(line)
+
+
+
+for line in corpus:
+    if line and '?' in line[0]:
+        print line
+
+
+starting_words = [line[0] for line in self.corpus if line]
+
